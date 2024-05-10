8cc3ead2ffddd68c13621b26efcbca4f2ea7e0dd
Lucerna00 <pjs3859811@naver.com>
2024-05-10 20:05:40 +0900

    feat: create optimization model

--- apps/optimizations/models.py
+++ apps/optimizations/models.py
@@ -20,10 +20,35 @@ class State(models.Model):
     class Meta:
         # 다중 기본키 설정
         constraints = [
-            models.UniqueConstraint(fields=['sequence', 'user', 'vertiport'], name='composite primary key'),
+            models.UniqueConstraint(fields=['sequence', 'user', 'vertiport'], name='state_composite_primary_key'),
         ]
+
         # DB 테이블 이름
         db_table = 'state'
 
     def __str__(self):
-        return f'{self.vertiport.name}.{self.sequence}'
+        return f'{self.state.user.id}.{self.vertiport.name}.{self.sequence}'
+
+
+class Optimization(models.Model):
+    weight = models.DecimalField(max_digits=3, decimal_places=2, null=False, blank=False)
+    fato_in_UAM = models.SmallIntegerField(default=0, null=False, blank=False)
+    path_in_UAM = models.SmallIntegerField(default=0, null=False, blank=False)
+    gate_UAM = models.SmallIntegerField(default=0, null=False, blank=False)
+    path_out_UAM = models.SmallIntegerField(default=0, null=False, blank=False)
+    fato_out_UAM = models.SmallIntegerField(default=0, null=False, blank=False)
+    gate_UAM_psg = models.SmallIntegerField(default=0, null=False, blank=False)
+    waiting_room_psg = models.SmallIntegerField(default=0, null=False, blank=False)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    class Meta:
+        # 다중 기본키 설정
+        constraints = [
+            models.UniqueConstraint(fields=['weight', 'state'], name='optimization_composite_primary_key'),
+        ]
+
+        # DB 테이블 이름
+        db_table = 'optimization'
+
+    def __str__(self):
+        return f'{self.state.user.id}.{self.state.vertiport.name}.{self.state.sequecne}.{self.weight}'
