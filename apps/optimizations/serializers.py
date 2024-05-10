e154e828f4ceb88699641cb2d55af6ac177f38a1
Lucerna00 <pjs3859811@naver.com>
2024-05-10 20:06:19 +0900

    feat: add optimization serializer

--- apps/optimizations/serializers.py
+++ apps/optimizations/serializers.py
@@ -1,8 +1,14 @@
-from .models import State
+from .models import State, Optimization
 from rest_framework import serializers
 
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
+        fields = '__all__'
+
+
+class OptimizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Optimization
         fields = '__all__'
